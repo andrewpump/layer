263bc7430d9d14364f190690d86310ec9a76c161
kapil <duckatgit@gmail.com>
2023-04-13 19:07:19 +0530

    Show error UI component if keys are not validated

--- layer-react-npm-package/dist/esm/index.js
+++ layer-react-npm-package/dist/esm/index.js
@@ -157,8 +157,34 @@ var EnvironmentError = function (_a) {
             React.createElement(Text, { className: "text-style", label: "Please add these keys to your process file to correct the error" }))));
 };
 
+function validateKeys() {
+  // Read keys from .env file with another project that is using this npm package for validation
+  const apiKey = process.env.REACT_APP_OPEN_AI_API_KEY;
+  const sdkKey = process.env.REACT_APP_LAYER_SDK_KEY;
+
+  if (
+    !apiKey ||
+    !sdkKey ||
+    apiKey !== "AB12C3D4-E5FG-67H8-91J0-KLMN120P3Q45" ||
+    sdkKey !== "sk_live_51Jx6f7gh8iL9a1b2c3d4e5F"
+  ) {
+    console.error("API key and/or SDK key not found in environment variables");
+    return {
+      error: true
+    };
+  }
+
+  return {
+    error: false,
+  };
+}
+
 var AiAssistant = function (_a) {
     var itemList = _a.itemList, color = _a.color, image = _a.image;
+    var error = validateKeys().error;
+    if (error) {
+        return React.createElement(EnvironmentError, { color: "#FF0000" });
+    }
     var _b = useState(false), showPopUp = _b[0], setShowPopUp = _b[1];
     var _c = useState(false), showDetails = _c[0], setShowDetails = _c[1];
     var _d = useState(false), showEnvError = _d[0]; _d[1];
